cfbd52f5c4599d629b5ad620dea5e2839f662645
Dimitar Stanchev Stefanov <dsstefanov2@gmail.com>
2018-12-27 23:43:55 +0100

    fixed access token interceptor

--- src/app/auth/interceptors/auth.interceptor.ts
+++ src/app/auth/interceptors/auth.interceptor.ts
@@ -1,19 +1,24 @@
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LocalStorageService } from '../../utils/LocalStorageService';
+import { AccessToken } from '../../models/AccessToken';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private lsService: LocalStorageService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (localStorage.curentUser || localStorage.accessToken) {
-      const currentUser = localStorage.currentUser || { accessToken: localStorage.accessToken};
+    if (this.lsService.accessToken && Object.keys(this.lsService.accessToken) > 0) {
+      const accessToken: AccessToken = this.lsService.accessToken;
       req = req.clone({
         setHeaders: {
-          Authorization: `Bearer ${currentUser.accessToken}`,
-          'refresh-token' : currentUser['refresh-token']
+          Authorization: `Bearer ${accessToken.token}`,
+          'refresh-token' : accessToken.refreshToken
         }
       });
     }
+    console.log(req);
     return next.handle(req);
   }
 }
