ca956bb7d94a6fa20f504392c4566b2529c98b05
Dimitar Stanchev Stefanov <dsstefanov2@gmail.com>
2018-12-30 18:36:21 +0100

    added a function that replaces :propertyId with the actual Id options object must follow {propertyId: String} format

--- src/app/utils/Urls.ts
+++ src/app/utils/Urls.ts
@@ -1,4 +1,5 @@
 import { constants } from './Constants';
+import { BPMError } from './BPMError';
 const baseUrl = constants.baseUrl;
 export class Urls {
   private urls: Map<string, string> = new Map();
@@ -8,5 +9,36 @@ export class Urls {
   }
   getUrl(resourceName: string) {
     return this.urls.get(resourceName);
+  }
+
+  getUrlWithParams(resourceName: string, options: Object) {
+    Object.keys(options).forEach(key => {
+      if (options.hasOwnProperty(key) && typeof options[key] !== 'string') {
+        throw new BPMError('URL options can only contain string values',
+          'getUrlWithParams', undefined);
+      }
+    });
+    const url = this.getUrl(resourceName);
+    const baseUrlLength = baseUrl.length;
+    let iterateFlag = true;
+    while (iterateFlag) {
+      const indexOfColon = url.indexOf(':', baseUrlLength);
+      let indexOfSlash = url.indexOf('/', indexOfColon);
+      if (indexOfColon === -1) {
+        iterateFlag = false;
+        break;
+      }
+      if (indexOfSlash === -1) {
+        indexOfSlash = url.length;
+      }
+      const propertyToReplace = url.substr(indexOfColon + 1, indexOfSlash - indexOfColon - 1);
+      const colonWithPropertyToReplace = url.substr(indexOfColon, indexOfSlash - indexOfColon);
+      if (options.hasOwnProperty(propertyToReplace)) {
+        url.replace(colonWithPropertyToReplace, options[propertyToReplace]);
+      } else {
+        throw new BPMError('A parameter is missing from the URL', 'Missing parameter', undefined);
+      }
+    }
+    return url;
   }
 }
